f895709875ac41ecf24da7ca0f040d4370331565
Huali Zhao <118955868+Huali2022@users.noreply.github.com>
2026-07-22 22:06:39 +0800

    Update main.R

--- main.R
+++ main.R
@@ -24,42 +24,17 @@ w = W_int[,1]
 pr = H_fn(beta_real[1] + beta_real[2]*X_int)
 y = c()
 for(i in 1:n){y[i] = rbinom(1,size = 1,prob = pr[i])}
-
-# 2.estimate nuisance parameters, misclassification matrix A_box and probability vector p------------
 ## define cut points
 J = 5 # categorize W into five categories based on quintiles
 C = rep(0, J-1)
 for(j in 1:(J-1)){C[j] = quantile(w,j/J)} 
-## estimate lambda in Box-Cox transformation
-b <- boxcox(lm(w ~ 1))
-lam_hat <- b$x[which.max(b$y)]  
-## estimate mean_box_x, s2_u, s2_box_x
-box_W_int = matrix(0,n,r)
-for(j in 1:r){
-  box_W_int[,j] = Box(W_int[,j],lam_hat)
-}
-box_W_int = na.omit(box_W_int)
-row_mean_box_w = apply(box_W_int, 1, mean) 
-mean_box_x_hat = mean(row_mean_box_w) 
-s2_box_w = apply(box_W_int, 1, var) 
-s2_u_hat = mean(s2_box_w) 
-s2_box_x_hat = max(mean((row_mean_box_w - mean_box_x_hat) ^ 2) - s2_u_hat/r, 
-                   0.2*(mean((row_mean_box_w - mean_box_x_hat)^2))) 
-## estimate misclassification matrix A_box and probability vector p
-a = rep(0,(J+1)); a[1] = min(w); a[2:J] = C; a[J+1] = max(w)
-Ap = fn_box(mean_box_x_hat,s2_box_x_hat,s2_u_hat,lam_hat,a)
-A_box = Ap$A_box 
-p_hat = Ap$p_hat  
 
-# 3.estimate theta_J-theta1 using our psimex method------------
+# 2.estimate theta_J-theta1 using our simfex method------------
 ## input
 w = w # observations with measurement error, a n-dim vector
 y = y # response, a n-dim vector
-A = A_box # a misclassification matrix with J rows and J columns
-p = p_hat # a J-dim probability vector
 B.boot = 50 # Bootstrap number for variance estimation
-## run our psimex method
-fn_psimex(w,y,A,p,B.boot)
+## run our simfex method
+fn_simfex(w, y, B.boot)
 ## output
 ## the point estimator, variance estimator and p-value for theta_J-theta_1
-
